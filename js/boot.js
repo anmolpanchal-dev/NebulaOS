cb056e7ff12d9493bfbce49377f20a36b484f3b5
Anmol Panchal <panchalji9705@gmail.com>
2026-07-11 23:39:07 +0530

    Login Work

--- js/boot.js
+++ js/boot.js
@@ -24,19 +24,14 @@ window.addEventListener("load", () => {
     const loader = document.getElementById("loaderWrapper");
     const mainContent = document.getElementById("mainContent");
 
-    // Ek chhota sa artificial delay (e.g. 1.5 seconds) takki user loader ko dekh sake
-    // Agar turant hatana hai toh delay ko 0 kar sakte hain.
-    setTimeout(() => {
-        // 1. Loader ko smoothly fade out karein
+   setTimeout(() => {
         loader.classList.add("loader-fade-out");
 
-        // 2. Animation khatam hone ke baad loader ko completely remove karein aur page dikhayein
         setTimeout(() => {
             loader.style.display = "none";
             
-            // Main page ko display block karke fade-in karein
             mainContent.classList.add("show");
-        }, 500); // 500ms match karta hai CSS ke fade-out transition se
+        }, 500); 
         
-    }, 1500); // 1500ms = 1.5 seconds ka buffer delay
+    }, 1500); 
 });
